4ec97fd482cd82a12af857fb56492bfddc4190fd
Shiva Kiran Reddy <shivakiranreddy@gmail.com>
2022-02-28 00:10:00 +0530

    added save model

--- src/training.py
+++ src/training.py
@@ -4,7 +4,7 @@ import argparse
 from sklearn import metrics
 from utils.common import read_config
 from utils.data_mgmt import get_data
-from utils.model import  create_model
+from utils.model import  create_model, save_model
 
 
 def training(config_path):
@@ -15,16 +15,27 @@ def training(config_path):
 
     LOSS_FUNCTION = config['params']['loss_function']
     OPTIMIZER = config['params']['optimizer']
-    METRICS = config["params"]["metrics"]
+    METRICS = [config["params"]["metrics"]]
 
     NUM_CLASSES = config["params"]["num_classes"]
 
     model = create_model(LOSS_FUNCTION,OPTIMIZER, METRICS, NUM_CLASSES )
 
-    EPOCHS = 30
+    EPOCHS = config['params']['epochs']
+
     VALIDATION = (X_valid, y_valid)
 
     history = model.fit(X_train, y_train, epochs = EPOCHS, validation_data = VALIDATION)
+
+    artifacts_dir = config['artifacts']['artifacts_dir']
+    model_dir = config['artifacts']['model_dir']
+
+    model_dir_path = os.path.join(artifacts_dir, model_dir)
+    os.makedirs(model_dir_path, exist_ok=True)
+
+    model_name = config['artifacts']['model_name']
+    save_model(model, model_name, model_dir_path)
+
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
